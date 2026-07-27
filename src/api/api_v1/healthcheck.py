e6f9a12a17c3fef07e1335cc105ef837b2d62d40
arthur <amahmetov1998@yandex.ru>
2026-07-27 16:41:58 +0500

    rm unused api

--- src/api/api_v1/healthcheck.py
+++ src/api/api_v1/healthcheck.py
@@ -9,12 +9,3 @@ router = APIRouter()
 @router.get("/healthcheck")
 async def healthcheck() -> dict[str, str]:
     return {"status": "ok"}
-
-
-@router.get("/test-external")
-async def external(
-    client: PhoneClient = Depends(get_phone_client),
-):
-    response = await client.client.get("/healthcheck")
-
-    return response.json()
